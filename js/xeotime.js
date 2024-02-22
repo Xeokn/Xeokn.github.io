5ce5fc31d5c257f3bb6323d88869a08ebbbd0ae1
Xeokn <74164454+Xeokn@users.noreply.github.com>
2024-02-22 19:43:08 +1000

    Live Indicator?

---
+++ js/xeotime.js
@@ -0,0 +1,11 @@
+fetch("https://decapi.me/twitch/uptime/xeokn/?offline_msg=notstreaming")
+    .then(resp => resp.text())
+    .then(status => {
+        if (status != "notstreaming") {
+            //LIVE
+            document.querySelector(".live-notif").style.visibility = "visible";
+        } else {
+            //Not live, Go touch grass or something.
+            document.querySelector(".live-notif").style.visibility = "hidden";
+        }
+    });
