6eab6123d767812af1e5674f79a0c3c332443b87
Xeokn <74164454+Xeokn@users.noreply.github.com>
2024-02-23 00:13:59 +1000

    Favicon and Inverval timer for livecheck

--- js/xeotime.js
+++ js/xeotime.js
@@ -1,11 +1,15 @@
-fetch("https://decapi.me/twitch/uptime/xeokn/?offline_msg=notstreaming")
-    .then(resp => resp.text())
-    .then(status => {
-        if (status != "notstreaming") {
-            //LIVE
-            document.querySelector(".live-notif").style.visibility = "visible";
-        } else {
-            //Not live, Go touch grass or something.
-            document.querySelector(".live-notif").style.visibility = "hidden";
-        }
-    });
+const liveCheck = setInterval(liveNotif, 60000)
+
+function liveNotif() {
+    fetch("https://decapi.me/twitch/uptime/xeokn/?offline_msg=notstreaming")
+        .then(resp => resp.text())
+        .then(status => {
+            if (status != "notstreaming") {
+                //LIVE
+                document.querySelector(".live-notif").style.visibility = "visible";
+            } else {
+                //Not live, Go touch grass or something.
+                document.querySelector(".live-notif").style.visibility = "hidden";
+            }
+        });
+};
